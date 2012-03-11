9ed3a8d596bc09e65cd0a7cc1590ac9cebee9e9d
TwistedUmbrella <twistedumbrella@gmail.com>
2012-03-11 04:43:24 -0400

    Remove secondary camera configuration from local device

--- mecha/mecha-vendor-blobs.mk
+++ mecha/mecha-vendor-blobs.mk
@@ -18,11 +18,18 @@
 
 PRODUCT_COPY_FILES += \
     vendor/htc/mecha/system/lib/libaudioalsa.so:obj/lib/libaudioalsa.so \
+    vendor/htc/mecha/system/lib/libcamera.so:obj/lib/libcamera.so \
+    vendor/htc/mecha/system/lib/libmmjpeg.so:obj/lib/libmmjpeg.so \
+    vendor/htc/mecha/system/lib/liboemcamera.so:obj/lib/liboemcamera.so \
     vendor/htc/mecha/system/lib/libril.so:obj/lib/libril.so \
     vendor/htc/mecha/system/lib/libv8.so:obj/lib/libv8.so
 
 # All the blobs necessary for mecha
 PRODUCT_COPY_FILES += \
+    vendor/htc/mecha/system/lib/libcamera.so:/system/lib/libcamera.so \
+    vendor/htc/mecha/system/lib/libmmipl.so:/system/lib/libmmipl.so \
+    vendor/htc/mecha/system/lib/libmmjpeg.so:/system/lib/libmmjpeg.so \
+    vendor/htc/mecha/system/lib/liboemcamera.so:/system/lib/liboemcamera.so \
     vendor/htc/mecha/system/bin/akmd:/system/bin/akmd \
     vendor/htc/mecha/system/bin/awb_camera:/system/bin/awb_camera \
     vendor/htc/mecha/system/bin/bma150_usr:/system/bin/bma150_usr \
