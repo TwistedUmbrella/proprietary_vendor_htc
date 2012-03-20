e4a7491a0489fe0407e0a3ed755d60fc351927c0
TwistedUmbrella <twistedumbrella@gmail.com>
2012-03-20 12:08:35 -0400

    Remove charging binary to attempt resolving wall charging

--- shooter/shooter-vendor-blobs.mk
+++ shooter/shooter-vendor-blobs.mk
@@ -29,7 +29,6 @@ PRODUCT_COPY_FILES += \
 # All the blobs necessary for shooter
 PRODUCT_COPY_FILES += \
     vendor/htc/shooter/proprietary/bin/bma150_usr:/system/bin/bma150_usr \
-    vendor/htc/shooter/proprietary/bin/charging:/system/bin/charging \
     vendor/htc/shooter/proprietary/bin/htcbatt:/system/bin/htcbatt \
     vendor/htc/shooter/proprietary/bin/htc_ebdlogd:/system/bin/htc_ebdlogd \
     vendor/htc/shooter/proprietary/bin/ip:/system/bin/ip \
