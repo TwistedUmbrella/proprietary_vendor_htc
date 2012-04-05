1b4fd0ed35d641cd32684f079c7b44f2d0b86738
TwistedUmbrella <twistedumbrella@gmail.com>
2012-04-04 21:53:13 -0400

    Remove unnecessary libraries and update webkit

--- shooter/shooter-vendor-blobs.mk
+++ shooter/shooter-vendor-blobs.mk
@@ -67,7 +67,6 @@ PRODUCT_COPY_FILES += \
     vendor/htc/shooter/proprietary/lib/libidl.so:/system/lib/libidl.so \
     vendor/htc/shooter/proprietary/lib/libmllite.so:/system/lib/libmllite.so \
     vendor/htc/shooter/proprietary/lib/libmlplatform.so:/system/lib/libmlplatform.so \
-    vendor/htc/shooter/proprietary/lib/libmm-color-convertor.so:/system/lib/libmm-color-convertor.so \
     vendor/htc/shooter/proprietary/lib/libmpl.so:/system/lib/libmpl.so \
     vendor/htc/shooter/proprietary/lib/libnetmgr.so:/system/lib/libnetmgr.so \
     vendor/htc/shooter/proprietary/lib/libOlaEngine.so:/system/lib/libOlaEngine.so \
